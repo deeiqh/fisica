6c58585edba14651f70ac2d41adc1278d808b2ec
k-st <kast46.15@gmail.com>
2018-04-12 10:29:39 -0500

    Add files via upload

--- paracaidas.cpp
+++ paracaidas.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main()
 {	
-	int lim = 13;
+	
 	double v, t;
-	double m = 68.1, c = 12.5;
+	double m = 68.1*2, c = 12.5;
 	fstream tabla;
 
 	tabla.open("data.txt", fstream::out);
@@ -16,19 +16,25 @@ int main()
 	double a, b, b2 = -1, e, vel_const = 0.001, inc = 0.1;//INCREMENTO
 	double v0 = 0, t0 = 0;
 	int pasos =0;
-	for(float t=0; t < lim; t += inc){
-		a = 53.39*(1-exp(-0.18355*t));
-		tabla << t << '\t' << a;
+	for(float t=0; ; t += inc){
+		a = 9.8*(m/c)*(1-exp(-(c/m)*t));
+		tabla<< "\t"<< t << "\t\t" << a;
 		if(t == 0){
-			b = 0;
+			b = 0; e = 0; 
 		}
-		else
+		else{
 			b = v0 + (9.8-(c/m*v0))*(inc);
+			e = ((b-a)/a)*100;
+		}
 		v0 = b;
-		tabla << '\t' << b;
-		e = ((b-a)/a)*100;
-		tabla << '\t' << e << " %\n";
-		if(b2 == b ) break;
+		tabla << "\t\t" << b;		
+		tabla << "\t\t" << e << " %\n";
+		if(b2 == b ){cerr<< "ok";
+			tabla<< "\t"<< t << "\t\t" << a;
+			tabla << "\t\t" << b;		
+			tabla << "\t\t" << e << " dfsdfdf%\n";
+			break;		
+		}
 		b2 = b;
 		pasos++;
 		cout << "pasos... " << pasos << '\n';
