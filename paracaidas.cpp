d479463974e8cd4167790e0c19b8ba047f1aceac
k-st <kast46.15@gmail.com>
2018-04-12 19:30:59 -0500

    Add files via upload

--- paracaidas.cpp
+++ paracaidas.cpp
@@ -11,8 +11,8 @@ int main()
 
 	tabla.open("data.txt", fstream::out);
 	
-	double tiempo = 0, analitico = 0, aprox = 0, error = 0, incremento = 1;
-	double v0 = 0, m = 68.1*2, c = 12.5;
+	double tiempo = 0, analitico = 0, aprox = 0, error = 0, incremento = 2;
+	double v0 = 0, m = 68.1, c = 12.5;
 	int pasos =0;
 	
 	tabla<< "\t"<< tiempo << "\t\t" << analitico << "\t\t" << aprox << "\t\t" << error << "%\n";
@@ -20,22 +20,23 @@ int main()
 	for(tiempo=incremento; ; tiempo += incremento){
 	
 		analitico = 9.8*(m/c)*(1-exp(-(c/m)*tiempo));
-		analitico = truncf(analitico*100)/100;
-
+		analitico = truncf(analitico*1000)/1000;
+		
 		aprox = aprox + (9.8-(c/m*aprox))*(incremento);
-		aprox = truncf(aprox*100)/100;
 		
 		error = ((aprox-analitico)/analitico)*100;
                 error = truncf(error*1000)/1000;
                 
-		tabla<< "\t"<< tiempo << "\t\t" << analitico << "\t\t" << aprox << "\t\t" << error << "%\n";
-		
-		if(abs(aprox-analitico) == 0) break;	
+		tabla<< "\t"<< tiempo << "\t\t" << analitico << "\t\t" <<  truncf(aprox*1000)/1000 << "\t\t" << error << "%\n";
 		
 		pasos++;
-		cout << "pasos... " << pasos << '\n';
+		cerr << aprox;
+		if(abs(aprox-analitico) <= 0.001) break;	
+		
+				
 	}
 	
+	cout << "pasos... " << pasos << '\n';
 	tabla.close();
 	return 0;
 }
