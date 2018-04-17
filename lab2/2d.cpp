1d7a521d35fc70206f2deafcbf7884848a68e47d
k-st <kast46.15@gmail.com>
2018-04-17 16:22:23 -0500

    Update 2d.cpp

--- lab2/2d.cpp
+++ lab2/2d.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int main()
 {
-	double v0 = 25 , theta = 37, m = 0.1, d = 0.06, c = 12.5, p = 1.225, g = 9.8;
+	double v0 = 25 , theta = 37, m = 0.1, d = 0.06, c = 0.1, p = 1.225, g = 9.8;
 	double a = 3.1415*(d/2)*(d/2);
 	double ax, ay;
 	double x = 0, y = 0, vx, vy;
@@ -22,12 +22,16 @@ int main()
 	arch.open("2d.txt", fstream::out);
 	
 	for(double t = 0; t != 30; t++){
-		ax = 0.5/m*c*a*p*v0*v0*cos(theta);
-		vx = (v0 + ax*t)*cos(theta);
-		vy = (v0 + ax*t)*sin(theta);
-		x = vx*t - 0.5*(0.5/m*c*a*p*v0*v0*cos(theta))*t*t;
-		y = vy*t - 0.5*(g+(0.5/m*c*a*p*v0*v0*sin(theta)))*t*t;
-		arch << t << ' '<<x<<' '<<y<<' '<<vx<<' '<<vy<<' '<<'\n';
+		ax = -0.5/m*c*a*p*v0*v0*cos(theta);
+		ay = -1*( g + (0.5/m*c*a*p*v0*v0*sin(theta)) );
+
+		vx = v0*cos(theta) + ax*t;
+		vy = v0*sin(theta) + ay*t;
+
+		x = v0*cos(theta)*t + 0.5*(ax)*t*t;
+		y = v0*sin(theta)*t + 0.5*(ay)*t*t;
+
+		arch << t << '\t'<<x<<'\t'<<y<<'\t'<<vx<<'\t'<<vy<<'\t'<<'\n';
 	}
 	arch.close();
 }
